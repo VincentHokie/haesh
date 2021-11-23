5d6eb19d6836a8f94181a33f27c3ffce98c43548
Vincent Asante <vincenthokie@gmail.com>
2021-11-23 17:45:06 +0300

    Small readability change

--- haesh/haesh.py
+++ haesh/haesh.py
@@ -38,7 +38,12 @@ class Haesh(object):
         for block_index in last_cipherblock:
             first_index = (block_index + self.iteration) % self._expansion_value
             # this addition needs to be mod 256 so that we pick a value within the array
-            expanded_ords.extend([self.expansion_permutation[first_index], self.expansion_permutation[(first_index + 1) % self._expansion_value], self.expansion_permutation[(first_index + 2) % self._expansion_value], self.expansion_permutation[(first_index + 3) % self._expansion_value]])
+            expanded_ords.extend([
+                self.expansion_permutation[first_index],
+                self.expansion_permutation[(first_index + 1) % self._expansion_value],
+                self.expansion_permutation[(first_index + 2) % self._expansion_value],
+                self.expansion_permutation[(first_index + 3) % self._expansion_value]
+            ])
 
         # ensure next iteration creates an offset in the values we pick from
         # expansion_permutation for expansion
